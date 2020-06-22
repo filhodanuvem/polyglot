fe6d837c39cd490b6633c48b4fdbb7e86dedd640
Claudson Oliveira <claudsonweb@gmail.com>
2020-06-22 01:59:53 +0100

    Add async way

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var limit = 4
+var tempPath = "/Users/cloudson/sources/github/polyglot/temp"
 
 func main() {
 	// log.SetOutput(ioutil.Discard)
@@ -16,7 +17,7 @@ func main() {
 		panic(err)
 	}
 
-	stats := getStatisticsSync(repos)
+	stats := getStatisticsAsync(repos)
 	log.Printf("%+v", stats.FirstLanguages(5))
 }
 
@@ -25,7 +26,7 @@ func getStatisticsSync(repos []string) repository.Statistics {
 	var resultStats repository.Statistics
 	c := 0
 	for i := range repos {
-		path, err := gh.Download(repos[i], "/Users/cloudson/sources/github/polyglot/temp")
+		path, err := gh.Download(repos[i], tempPath)
 		if err != nil {
 			panic(err)
 		}
@@ -35,12 +36,45 @@ func getStatisticsSync(repos []string) repository.Statistics {
 		if err != nil {
 			panic(err)
 		}
-		// fmt.Println(resultStats)
 		resultStats.Merge(&stats)
 		c++
 		if c == limit {
 			break
 		}
+	}
+
+	return resultStats
+}
+
+func getStatisticsAsync(repos []string) repository.Statistics {
+	gh := github.Downloader{}
+	statsChan := make(chan repository.Statistics, limit)
+	c := 0
+	for i := range repos {
+		go func(repo string) {
+			path, err := gh.Download(repo, tempPath)
+			if err != nil {
+				panic(err)
+			}
+
+			files := repository.GetFiles(path)
+			stats, err := repository.GetStatistics(files)
+			if err != nil {
+				panic(err)
+			}
+			statsChan <- stats
+		}(repos[i])
+		c++
+		if c == limit {
+			break
+		}
+	}
+
+	var resultStats repository.Statistics
+	select {
+	case stats := <-statsChan:
+		resultStats.Merge(&stats)
+		break
 	}
 
 	return resultStats
