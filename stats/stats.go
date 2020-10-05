88d90c65f8016a84dfa991c21e1bd810f454f14f
Gustavo Santana <gustavofelipe68@gmail.com>
2020-10-05 20:00:49 -0300

    separete statistics from cmd package to stats package

---
+++ stats/stats.go
@@ -0,0 +1,88 @@
+package stats
+
+import (
+	"os"
+
+	"github.com/filhodanuvem/polyglot/github"
+	"github.com/filhodanuvem/polyglot/repository"
+	log "github.com/sirupsen/logrus"
+)
+
+var limitRepos = 100
+var limitChannels = 30
+
+func GetStatisticsAsync(tempPath string, repos []string, l *log.Logger) repository.Statistics {
+	statsChan := make(chan repository.Statistics, limitRepos)
+	terminated := make(chan bool, limitChannels)
+	count := 0
+
+	for i := range repos {
+		go func(repo string) {
+			defer func() {
+				terminated <- true
+			}()
+			stats, err := getStatsFromRepo(repo, tempPath, l)
+			if err != nil {
+				l.Error(err)
+				return
+			}
+			statsChan <- stats
+		}(repos[i])
+		count++
+		if count == limitRepos {
+			break
+		}
+	}
+
+	l.Println(">>>>>> Waiting for terminated")
+	for i := 0; i < count; i++ {
+		select {
+		case <-terminated:
+		}
+	}
+	close(statsChan)
+
+	l.Println(">>>>>>> Waiting for statsChan")
+	var resultStats repository.Statistics
+	for range statsChan {
+		stats := <-statsChan
+		resultStats.Merge(&stats)
+	}
+
+	return resultStats
+}
+
+func GetStatisticsSync(tempPath string, repos []string, l *log.Logger) repository.Statistics {
+	var resultStats repository.Statistics
+	c := 0
+	for i := range repos {
+		stats, err := getStatsFromRepo(repos[i], tempPath, l)
+		if err != nil {
+			l.Error(err)
+			continue
+		}
+		resultStats.Merge(&stats)
+		c++
+		if c == limitRepos {
+			break
+		}
+	}
+
+	return resultStats
+}
+
+func getStatsFromRepo(repo, tempPath string, l *log.Logger) (repository.Statistics, error) {
+	if _, err := os.Stat(tempPath); os.IsNotExist(err) {
+		os.MkdirAll(tempPath, os.ModePerm)
+	}
+	gh := github.Downloader{}
+	path, err := gh.Download(repo, tempPath, l)
+	if err != nil {
+		l.Error(err)
+	}
+
+	files := repository.GetFiles(path, l)
+	stats, err := repository.GetStatistics(files)
+
+	return stats, err
+}
