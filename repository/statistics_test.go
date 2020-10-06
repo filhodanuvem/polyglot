50b140d6854830d35137390e13929c814d7f0120
Gustavo Santana <gustavofelipe68@gmail.com>
2020-10-05 22:31:16 -0300

    Export Counter and make it JSON compatible; Add Languages to excludeList

--- repository/statistics_test.go
+++ repository/statistics_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func est_MergeOneStatsShouldCountCorrectly(t *testing.T) {
 	stats := Statistics{
-		counters: []counter{counter{lang: "PHP", counter: 2}},
+		counters: []Counter{Counter{Lang: "PHP", Counter: 2}},
 		langs:    map[string]int{"PHP": 0},
 	}
 
@@ -12,19 +12,19 @@ func est_MergeOneStatsShouldCountCorrectly(t *testing.T) {
 	resultStats.Merge(&stats)
 	langs := resultStats.FirstLanguages(1)
 
-	if langs[0].lang != "PHP" && langs[0].counter != 1 {
+	if langs[0].Lang != "PHP" && langs[0].Counter != 1 {
 		t.Errorf("Expected PHP:1 found %+v", langs)
 	}
 }
 
 func Test_MergeTwoStatsShouldCountCorrectly(t *testing.T) {
 	stats1 := Statistics{
-		counters: []counter{counter{lang: "PHP", counter: 2}},
+		counters: []Counter{Counter{Lang: "PHP", Counter: 2}},
 		langs:    map[string]int{"PHP": 0},
 	}
 
 	stats2 := Statistics{
-		counters: []counter{counter{lang: "Go", counter: 3}, counter{lang: "PHP", counter: 5}},
+		counters: []Counter{Counter{Lang: "Go", Counter: 3}, Counter{Lang: "PHP", Counter: 5}},
 		langs:    map[string]int{"Go": 0, "PHP": 1},
 	}
 
@@ -33,11 +33,11 @@ func Test_MergeTwoStatsShouldCountCorrectly(t *testing.T) {
 	resultStats.Merge(&stats2)
 	langs := resultStats.FirstLanguages(2)
 
-	if langs[0].lang != "PHP" && langs[0].counter != 6 {
+	if langs[0].Lang != "PHP" && langs[0].Counter != 6 {
 		t.Errorf("Expected PHP:6 found %+v", langs)
 	}
 
-	if langs[1].lang != "Go" && langs[0].counter != 3 {
+	if langs[1].Lang != "Go" && langs[0].Counter != 3 {
 		t.Errorf("Expected PHP:6 found %+v", langs)
 	}
 }
