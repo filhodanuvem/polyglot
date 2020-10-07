029ce25115c8d4df7f45a940741c66c0fd9a4b6f
Esron Dtamar <esron.dtamar@gmail.com>
2020-10-06 23:08:49 -0300

    test the logs and invalid path

--- repository/files_test.go
+++ repository/files_test.go
@@ -3,15 +3,28 @@ package repository
 import (
 	"testing"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetFiles(t *testing.T) {
-	l := log.New()
+	l, hook := test.NewNullLogger()
 
 	files := GetFiles("../testdata", l)
 
 	if files == nil {
 		t.Errorf("Should not return nil if the passed a valid files path")
+	}
+
+	// Test the GetFiles logs output
+	assert.Equal(t, 1, len(hook.Entries))
+	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
+	assert.Equal(t, "Listing files of repo", hook.LastEntry().Message)
+
+	files = GetFiles("", l)
+
+	if files != nil {
+		t.Errorf("Should return nil if the path leads to a invalid files folder")
 	}
 }
